9b1ec516d19602bc146718efbdd4a3f82d5637f4
geddadasuresh84326 <geddadasuresh6@gmail.com>
2024-02-18 18:57:06 +0530

    app.py updated

--- app.py
+++ app.py
@@ -37,4 +37,6 @@ def chat():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+    app.run(host="0.0.0.0", port= 8080) # for deployment 
+    # app.run(host="0.0.0.0", port= 8080, debug= True) # for local run
+    # app.run(host="127.0.0.1", port= 8080, debug= True) # for local run
